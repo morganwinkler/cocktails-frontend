56c384d7a66d44a762106641936c6f3fcde50354
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-10 11:31:57 -0500

    Defines data in content
    Uses props to pass to child element- recipes index. Ignores prop validations for entire file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
@@ -32,29 +33,20 @@ function DrinksNew() {
   );
 }
 
-function DrinksIndex() {
+function DrinksIndex(props) {
   return (
     <div id="drinks-index">
-          <h3>All Cocktails</h3>
-          <div className="drinks">
-            <h2>Drink One</h2>
-            <img src="https://media.istockphoto.com/id/1216205052/photo/blue-old-fashioned-cocktail-with-cherry-on-the-rustic-background.webp?b=1&s=170667a&w=0&k=20&c=06KnDW7kDynqlpnR644IYljQ05aaTGD5Wzo_ESpfvNA=" alt="" />
-            <p>Mixologist:</p>
-            <button>More Info!</button>
-          </div>
-          <div className="drinks">
-            <h2>Drink Two</h2>
-            <img src="https://media.istockphoto.com/id/1167570076/photo/tropical-cocktails-near-swimming-pool.webp?b=1&s=170667a&w=0&k=20&c=P_tn8hl_V94CVR3l_72-DVyaKTETIHkCKnZCr5Krc18=" alt="" />
-            <p>Mixologist:</p>
-            <button>More Info!</button>
-          </div>
-          <div className="drinks">
-            <h2>Drink Three</h2>
-            <img src="https://media.istockphoto.com/id/1172084897/photo/women-raising-a-glasses-of-spritz-at-the-dinner-table.webp?b=1&s=170667a&w=0&k=20&c=pluXQFh8t8CraUEFUSB7XE6HV38M7UqIzYaUqcbPykQ=" alt="" />
-            <p>Mixologist:</p>
-            <button>More Info!</button>
-          </div>
+      <h3>All the Cocktails!</h3>
+      {/* loop of defined recipe data props from the parent component */}
+      {props.drinks.map((drink) => (
+        <div key={drink.id}>
+          <h2>{drink.name}</h2>
+          <img src={drink.image_url} alt="drink pics" />
+          <p>Mixologist: {drink.mixologist} </p>
+          <button>More Info!</button>
         </div>
+      ))}
+    </div>
   );
 }
 
@@ -67,10 +59,31 @@ function Footer() {
 }
 
 function Content() {
+  let drinks = [
+    {
+      id: 1,
+      name: "Drink One",
+      mixologist: "Mike",
+      image_url: "https://media.istockphoto.com/id/1216205052/photo/blue-old-fashioned-cocktail-with-cherry-on-the-rustic-background.webp?b=1&s=170667a&w=0&k=20&c=06KnDW7kDynqlpnR644IYljQ05aaTGD5Wzo_ESpfvNA=",
+    },
+    {
+      id: 2,
+      name: "Drink Two",
+      mixologist: "Mo",
+      image_url: "https://media.istockphoto.com/id/1167570076/photo/tropical-cocktails-near-swimming-pool.webp?b=1&s=170667a&w=0&k=20&c=P_tn8hl_V94CVR3l_72-DVyaKTETIHkCKnZCr5Krc18=",
+    },
+    {
+      id: 3,
+      name: "Drink Three",
+      mixologist: "Shayna",
+      image_url: "https://media.istockphoto.com/id/1172084897/photo/women-raising-a-glasses-of-spritz-at-the-dinner-table.webp?b=1&s=170667a&w=0&k=20&c=pluXQFh8t8CraUEFUSB7XE6HV38M7UqIzYaUqcbPykQ=",
+    },
+  ];
+
   return (
     <div>
       <DrinksNew />
-      <DrinksIndex />
+      <DrinksIndex drinks={drinks} />
     </div>
   );
 }
